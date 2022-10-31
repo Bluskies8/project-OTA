734054f1fdfd1c14853841135a05432a153aee38
Mitchell Arthur <mitchell2@mhs.stts.edu>
2022-10-31 23:20:31 +0700

    detail booking tour

--- resources/views/pages/backoffice/detailbookingtour.blade.php
+++ resources/views/pages/backoffice/detailbookingtour.blade.php
@@ -1,63 +1,120 @@
-@section('title')Data Diri Tour @endsection
-@extends('layout.users')
+@extends('layout.admins')
 
 @section('content')
-<meta name="csrf-token" content="{{ csrf_token() }}">
+@include('includes.datatables')
 
-<div class="container py-5">
-    <div class="table-responsive">
-        <table class="table table-bordered">
-            <thead>
-                <tr class="text-center">
-                    <th>Title</th>
-                    <th>Guest Name</th>
-                    <th>NIK</th>
-                    <th>Nomor HP</th>
-                    <th>Tipe</th>
-                </tr>
-            </thead>
-            <tbody>
-                @dump($data)
-                @foreach ($data as $item)
-                <tr class="row-kamar">
-                    <td class="fw-bolder bg-main" colspan="5">Kamar {{$loop->index+1}}</td>
-                </tr>
-                    @foreach ($item as $final)
-                        <tr class="row-data">
-                            <td class="text-end">{{$final['title']}}</td>
-                            <td>{{$final['name']}}</td>
-                            <td class="text-center">{{$final['nik']}}</td>
-                            <td class="text-center">{{$final['phone']}}</td>
-                            <td class="text-center">Adult</td>
+<div class="container">
+    <div class="my-4" style="font-size: 1.25rem;">
+        <i class="fas fa-arrow-left color-main me-4"></i>
+        <a class="me-3" href="#" style=" color: inherit!important; text-decoration: none!important;">CMS</a>
+        <i class="fas fa-chevron-right me-3"></i>
+        <a class="me-3" href="#" style=" color: inherit!important; text-decoration: none!important;">Product Management</a>
+        <i class="fas fa-chevron-right me-3"></i>
+        <a class="me-3" href="#" style=" color: inherit!important; text-decoration: none!important;">Tour</a>
+        <i class="fas fa-chevron-right me-3"></i>
+        <a class="me-3" href="#" style=" color: inherit!important; text-decoration: none!important;">Booking</a>
+    </div>
+    <div class="card">
+        <div class="card-body">
+            <h1>Table Booking Tour</h1>
+            <hr style="margin: 1rem -1rem;" />
+            <div class="p-3 data-penerbangan mb-3" style="background-image: linear-gradient(to top, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.5)), url(bg_search.jpg);background-repeat: no-repeat;background-size: cover;background-position: center center;border-radius: inherit;">
+                <div class="row">
+                    <div class="col-4 d-flex align-items-center" style="color: white;">
+                        <p class="ms-3 fs-4 fw-bold">Nama Tour</p>
+                    </div>
+                    <div class="col-4 d-flex align-items-center justify-content-evenly offset-4" style="color: white;">
+                        <div class="text-center"><i class="fas fa-sun" style="font-size: 1.25rem;"></i>
+                            <p>4 hari</p>
+                        </div>
+                        <div class="text-center"><i class="fas fa-moon"></i>
+                            <p>3 Malam</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="table-responsive">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr class="text-center">
+                            <th>Title</th>
+                            <th>Guest Name</th>
+                            <th>NIK</th>
+                            <th>Nomor HP</th>
+                            <th>Tipe</th>
+                            <th style="width: 51px;"></th>
                         </tr>
-                    @endforeach
-                @endforeach
-                {{-- <tr class="row-data">
-                    <td class="text-end">Mrs</td>
-                    <td>Larisa</td>
-                    <td class="text-center">1234567891</td>
-                    <td class="text-center">08123456789</td>
-                    <td class="text-center">Adult</td>
-                </tr>
-                <tr class="row-data">
-                    <td class="text-end">Mr</td>
-                    <td>Jimmy</td>
-                    <td class="text-center">1234567892</td>
-                    <td class="text-center">08123456789</td>
-                    <td class="text-center">Child</td>
-                </tr>
-                <tr class="row-kamar">
-                    <td class="fw-bolder bg-main" colspan="5">Kamar 2</td>
-                </tr>
-                <tr class="row-data">
-                    <td class="text-end">Mr</td>
-                    <td>Kevin</td>
-                    <td class="text-center">1234567890</td>
-                    <td class="text-center">08123456789</td>
-                    <td class="text-center">Adult</td>
-                </tr> --}}
-            </tbody>
-        </table>
+                    </thead>
+                    <tbody>
+                        {{-- @dump($data) --}}
+                        @foreach ($data as $item)
+                            <tr class="row-kamar">
+                                <td class="fw-bolder bgcolor-main" colspan="6">Kamar {{ $loop->index+1 }}</td>
+                            </tr>
+                            @foreach ($item as $final)
+                                <tr class="row-data">
+                                    <td class="text-end">{{ $final['title'] }}</td>
+                                    <td>{{ $final['name'] }}</td>
+                                    <td class="text-center">{{ $final['nik'] }}</td>
+                                    <td class="text-center">{{ $final['phone'] }}</td>
+                                    <td class="text-center">{{ $final['type'] }}</td>
+                                    <td class="cell-aksi"><button class="btn btn-primary btn-sm" type="button"><i class="fas fa-edit"></i></button></td>
+                                </tr>
+                            @endforeach
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
+{{--
+<div class="container">
+    <div class="my-5" style="font-size: 1.25rem;"><i class="fas fa-arrow-left color-main me-4"></i><a class="me-3" href="#">CMS</a><i class="fas fa-chevron-right me-3"></i><a class="me-3" href="#">Product Management</a><i class="fas fa-chevron-right me-3"></i><a class="me-3" href="#">Tour</a><i class="fas fa-chevron-right me-3"></i><a class="me-3" href="#">Booking</a></div>
+
+                    <tbody>
+                        <tr class="row-kamar">
+                            <td class="fw-bolder bgcolor-main" colspan="6">Kamar 1</td>
+                        </tr>
+                        <tr class="row-data">
+                            <td class="text-end">Mr</td>
+                            <td>Kevin</td>
+                            <td class="text-center">1234567890</td>
+                            <td class="text-center">08123456789</td>
+                            <td class="text-center">Adult</td>
+                            <td class="cell-aksi"><button class="btn btn-primary btn-sm" type="button"><i class="fas fa-edit"></i></button></td>
+                        </tr>
+                        <tr class="row-data">
+                            <td class="text-end">Mrs</td>
+                            <td>Larisa</td>
+                            <td class="text-center">1234567891</td>
+                            <td class="text-center">08123456789</td>
+                            <td class="text-center">Adult</td>
+                            <td class="cell-aksi"><button class="btn btn-primary btn-sm" type="button"><i class="fas fa-edit"></i></button></td>
+                        </tr>
+                        <tr class="row-data">
+                            <td class="text-end">Mr</td>
+                            <td>Jimmy</td>
+                            <td class="text-center">1234567892</td>
+                            <td class="text-center">08123456789</td>
+                            <td class="text-center">Child</td>
+                            <td class="cell-aksi"><button class="btn btn-primary btn-sm" type="button"><i class="fas fa-edit"></i></button></td>
+                        </tr>
+                        <tr class="row-kamar">
+                            <td class="fw-bolder bgcolor-main" colspan="6">Kamar 2</td>
+                        </tr>
+                        <tr class="row-data">
+                            <td class="text-end">Mr</td>
+                            <td>Kevin</td>
+                            <td class="text-center">1234567890</td>
+                            <td class="text-center">08123456789</td>
+                            <td class="text-center">Adult</td>
+                            <td class="cell-aksi"><button class="btn btn-primary btn-sm" type="button"><i class="fas fa-edit"></i></button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div> --}}
--- resources/views/pages/backoffice/report_tour_booking.blade.php
+++ resources/views/pages/backoffice/report_tour_booking.blade.php
@@ -3,68 +3,80 @@
 @section('content')
 @include('includes.datatables')
 
-	<div class="card col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" id="section-tour">
-		<div class="card-body">
-			<header>
-				<h3>1. Select Tour</h3>
-			</header>
-			<hr style="margin: 1rem -1rem;" />
+<div class="container">
+    <div class="my-4" style="font-size: 1.25rem;">
+        <i class="fas fa-arrow-left color-main me-4"></i>
+        <a class="me-3" href="#" style=" color: inherit!important; text-decoration: none!important;">CMS</a>
+        <i class="fas fa-chevron-right me-3"></i>
+        <a class="me-3" href="#" style=" color: inherit!important; text-decoration: none!important;">Product Management</a>
+        <i class="fas fa-chevron-right me-3"></i>
+        <a class="me-3" href="#" style=" color: inherit!important; text-decoration: none!important;">Tour</a>
+        <i class="fas fa-chevron-right me-3"></i>
+        <a class="me-3" href="#" style=" color: inherit!important; text-decoration: none!important;">Booking</a>
+    </div>
+    <div class="card col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" id="section-tour">
+        <div class="card-body">
+            <header>
+                <h3>1. Select Tour</h3>
+            </header>
 
-			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 form-group">
-				<input list="data-tour"  class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 form-group data-tour" >
-				<datalist id="data-tour">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 form-group mt-4">
+                <input list="data-tour" class="px-2 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 form-group data-tour" style="border-radius: .25rem;">
+                <datalist id="data-tour">
                     @foreach ($tour as $item)
                         <option value="{{$item->name}}" id = "{{$item->id}}"></option>
                     @endforeach
-				</datalist>
-			</div>
+                </datalist>
+            </div>
 
-			<header>
-				<h3>2. Select Depature Date</h3>
-			</header>
-			<hr style="margin: 1rem -1rem;" />
+            <hr style="margin: 1rem -1rem;" />
+            <header>
+                <h3>2. Select Depature Date</h3>
+            </header>
 
-			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 form-group">
-				<input list="tour-date"  class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 form-group data-date">
-				<datalist id="tour-date">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 form-group mt-4">
+                <input list="tour-date" class="px-2 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 form-group data-date" style="border-radius: .25rem;">
+                <datalist id="tour-date">
 
-				</datalist>
-			</div>
-		</div>
-		<div class="card-body">
-			<hr style="margin: 1rem -1rem;" />
-			<div class="table-responsive">
-				<table class="table" id="table-tours">
-					<thead>
-						<tr>
-							<th>No.</th>
-							<th>Booking Code </th>
-							<th>Customer</th>
-							{{-- <th>Product Tour</th>
-							<th>Tour Date</th> --}}
-							<th>Total (IDR)</th>
-							<th>Payment URL</th>
-							<th>Payment Status</th>
-							<th>Action</th>
-						</tr>
-					</thead>
-					<tbody>
+                </datalist>
+            </div>
+        </div>
+
+        <hr />
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table" id="table-tours">
+                    <thead>
+                        <tr>
+                            <th>No.</th>
+                            <th>Booking Code </th>
+                            <th>Customer</th>
+                            {{-- <th>Product Tour</th>
+                            <th>Tour Date</th> --}}
+                            <th>Total (IDR)</th>
+                            <th>Payment URL</th>
+                            <th>Payment Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
                         {{-- @foreach ($data as $item)
-						<tr id = "">
-							<td>{{$loop->index+1}}</td>
-							<td>{{$item->bookingCode}}</td>
-							<td>{{$item->customer->guest_name}}</td>
-							<td class="text-right">{{$item->total}}</td>
-							<td class="text-right">{{$item->payment_url}}</td>
-							<td class="text-right">{{$item->payment_status}}</td>
-							<td class="cell-action"><i class="fas fa-exclamation-circle text-primary"></i></td>
-						</tr>
+                        <tr id = "">
+                            <td>{{$loop->index+1}}</td>
+                            <td>{{$item->bookingCode}}</td>
+                            <td>{{$item->customer->guest_name}}</td>
+                            <td class="text-right">{{$item->total}}</td>
+                            <td class="text-right">{{$item->payment_url}}</td>
+                            <td class="text-right">{{$item->payment_status}}</td>
+                            <td class="cell-action"><i class="fas fa-exclamation-circle text-primary"></i></td>
+                        </tr>
                         @endforeach --}}
-					</tbody>
-				</table>
-			</div>
-		</div>
-	</div>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
 
 <script src="{{asset('js/backoffice/reportTour.js')}}"></script>
 @endsection
