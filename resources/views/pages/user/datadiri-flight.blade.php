ed8a6d495fa137689355272a45b37c1d0623dbef
Mitchell Arthur <mitchell2@mhs.stts.edu>
2022-11-10 21:39:10 +0700

    Update datadiri-flight.blade.php

--- resources/views/pages/user/datadiri-flight.blade.php
+++ resources/views/pages/user/datadiri-flight.blade.php
@@ -156,5 +156,24 @@
         </div>
     </div>
 </div>
+
+<div role="dialog" tabindex="-1" class="modal fade">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Modal Title</h4>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p>The content of your modal.</p>
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-light" type="button" data-bs-dismiss="modal">Close</button>
+                <button class="btn btn-primary" type="button">Save</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 <script src="{{asset('js/user/tour.js')}}"></script>
 @endsection
